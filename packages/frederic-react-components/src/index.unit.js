03778aca994c8e7911d7cf25815d8623592f58d5
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-06-18 16:44:28 +0100

    Edit tape test to check if components are exported and are in dependencies.

--- packages/frederic-react-components/src/index.unit.js
+++ packages/frederic-react-components/src/index.unit.js
@@ -1,10 +1,29 @@
 import test from 'tape';
 
-import * as exports from '.';
+import fs from 'fs';
+import path from 'path';
 
-test('index - ensure all items exported exist', assert => {
-  Object.entries(exports).forEach(([key, value]) => {
-    assert.notEqual(value, undefined, `${key} exported correctly`);
+import * as packageExports from './index';
+import pkg from '../package.json';
+
+test('index - ensure all items exported exist', () => {
+  // all exports are truthy
+  Object.keys(packageExports).map(exportName => {
+    assert(!!packageExports[exportName], `${exportName} is truthy`);
+  });
+  assert.end();
+});
+
+// https://stackoverflow.com/questions/18112204/get-all-directories-within-directory-nodejs?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory());
+
+test('Components exist in dependencies', () => {
+  // all componenents are dependencies of frederic-react-components
+  // if needed in future, can add an exclusion list/array to this test
+  const components = dirs(path.join(__dirname, '../../../components'));
+
+  components.map(component => {
+    assert(!!pkg.dependencies[`@frederic-react-components/${component}`], `${component} is in dependencies`);
   });
   assert.end();
 });
