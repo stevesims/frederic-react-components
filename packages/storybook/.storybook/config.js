d6c0b9dcc1764b15d813f7c0ce2644f722df4681
Tarandeep Singh Chauhan <Tarandeep.Chauhan-CIC-UK@ibm.com>
2018-06-18 16:22:24 +0100

    Lerna monorepo initial commit

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/react';
+import 'react-chromatic/storybook-addon';
 
 function loadStories() {
-  require('../stories');
+  require('../src/stories');
 }
 
 configure(loadStories, module);
