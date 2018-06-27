ffa94bdfa1b2d8ff55cf7a651311141680d85c8f
Gavin Orland <gavin@gavinorland.com>
2018-06-27 11:53:21 +0100

    Count - Replace unit test

---
+++ components/count/src/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Count from '.';
+
+describe('Count', () => {
+  let wrapper;
+
+  it('renders without crashing', () => {
+    wrapper = shallow(<Count value={4} />);
+  });
+
+  it('displays number passed in correctly', () => {
+    expect(wrapper.find('span').text()).toBe('4');
+  });
+});
