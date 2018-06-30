e3e0ab41ced877a755ed2ab3e783d09a56e0bf0e
Gavin Orland <gavin@gavinorland.com>
2018-06-30 21:32:08 +0100

    HeaderButton - Replace unit tests

---
+++ components/header-button/src/test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { createMatchers } from 'jest-emotion';
+import * as emotion from 'emotion';
+import Component from '.';
+import { LINK_COLOUR } from 'govuk-colours';
+
+expect.extend(createMatchers(emotion));
+
+describe('HeaderButton', () => {
+  let wrapper;
+
+  it('renders with no properties', () => {
+    wrapper = shallow(<Component />);
+  });
+
+  it('applies styles on active prop', () => {
+    wrapper = shallow(<Component active />).find('Button');
+    expect(wrapper).toHaveStyleRule('border-bottom-color', LINK_COLOUR);
+    expect(wrapper).toHaveStyleRule('border-top-color', LINK_COLOUR);
+    expect(wrapper).toHaveStyleRule('color', LINK_COLOUR);
+  });
+  
+  it('applies styles on disabled prop', () => {
+    wrapper = shallow(<Component disabled />).find('Button');
+    expect(wrapper).toHaveStyleRule('opacity', '0.35');
+    // Can't see a way of testing pseudo-selectors here...
+  });
+
+  it('matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
