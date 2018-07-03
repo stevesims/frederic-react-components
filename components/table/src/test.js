7d9dfe74e155a0fba021775252a0c3d593e10b3a
Gavin Orland <gavin@gavinorland.com>
2018-07-03 13:52:57 +0100

    Table test - Correct element names

--- components/table/src/test.js
+++ components/table/src/test.js
@@ -9,7 +9,7 @@ expect.extend(createMatchers(emotion));
 describe('Table', () => {
   const titles = ['Heading 1', 'Heading 2', 'Heading 3', 'Heading 4'];
   const rows = [
-    ['Content 1-1', 'Content 1-2', 'Content 3', 'Content 4'],
+    ['Content 1-1', 'Content 1-2', 'Content 1-3', 'Content 1-4'],
     ['Content 2-1', 'Content 2-2', 'Content 2-3', 'Content 2-4'],
     ['Content 3-1', 'Content 3-2', 'Content 3-3', 'Content 3-4'],
   ];
