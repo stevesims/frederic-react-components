883516c78eecfc203f81c1805c39aeebd83a1e9e
Gavin Orland <gavin@gavinorland.com>
2018-08-06 14:09:54 +0100

    ObjectTable - develop tests
    These are at 100% coverage but more could be tested.

--- components/object-table/src/test.js
+++ components/object-table/src/test.js
@@ -4,17 +4,21 @@ import { mount } from 'enzyme';
 import Component from '.';
 
 describe('ObjectTable', () => {
-  it('does render with no data', () => {
-    const wrapper = mount(<Component />);
-    expect(wrapper.html());
+  let fields = [
+    { key: 'one', heading: 'One' },
+    { key: 'two', heading: 'Two', transform: () => 'two' },
+  ];
+  let object = { one: 'test', two: 'test' };
+  const title = 'Heading';
+  let wrapper;
+
+  it('renders with defaults', () => {
+    wrapper = mount(<Component />);
+    expect(wrapper.exists()).toBe(true);
   });
+
   it('renders a table of data based on params', () => {
-    const fields = [
-      { key: 'one', heading: 'One' },
-      { key: 'two', heading: 'Two', transform: () => 'two' },
-    ];
-    const object = { one: 'test', two: 'test' };
-    const wrapper = mount(<Component fields={fields} object={object} />);
+    wrapper = mount(<Component fields={fields} object={object} />);
     const table = wrapper.find('Table');
     const rows = table.prop('rows');
   
@@ -22,47 +26,37 @@ describe('ObjectTable', () => {
   });
 
   it('optionally renders a heading for a table', () => {
-    const fields = [
+    fields = [
       { key: 'one', heading: 'One' },
       { key: 'two', heading: 'Two', transform: () => 'two' },
     ];
-    const object = { one: 'test', two: 'test' };
-    const wrapper = mount(<Component fields={fields} object={object} title="Heading" />);
+    object = { one: 'test', two: 'test' };
+    wrapper = mount(<Component fields={fields} object={object} title={title} />);
     expect(wrapper.contains('Heading')).toBe(true);
   });
 
   it('omits rows in table for null values', () => {
-    const fields = [
+    fields = [
       { key: 'one', heading: 'one' },
       { key: 'two', heading: 'two', transform: () => null },
       { key: 'three', heading: 'three'},
     ];
-    const object = { one: 'test', two: 'test', three: null };
-    const wrapper = mount(<Component fields={fields} object={object} />);
+    object = { one: 'test', two: 'test', three: null };
+    wrapper = mount(<Component fields={fields} object={object} />);
     const rows = wrapper.find('tr');
     
     expect(rows.length).toBe(1);
   });
 
-  it('does not render anything when rows have no values and hideWithNoValues is true', () => {
-    const fields = [
+  it('renders nothing when rows have no values and hideWithNoValues is true', () => {
+    fields = [
       { key: 'one', heading: 'one' },
       { key: 'two', heading: 'two', transform: () => null },
       { key: 'three', heading: 'three'},
     ];
-    const object = {};
-    const wrapper = mount(<Component fields={fields} object={object} />);   
+    object = {};
+    wrapper = mount(<Component fields={fields} object={object} hideWithNoValues />);   
     expect(wrapper.html()).toBe(null);
-  });
-
-  xit('renders one tbody row when no valid data if hideWithNoValues is not true', () => {
-    const fields = [
-      { key: 'one', heading: 'One' },
-      { key: 'two', heading: 'Two' },
-    ];
-    const object = {};
-    const wrapper = mount(<Component fields={fields} object={object} hideWithNoValues/>);  
-    expect(wrapper.find('tr').length).toBe(1);
   });
 });
 
