6d9684f8a79ea06d6c99c0773c12ae6e9d053b88
Toby Brancher <email@tobybrancher.net>
2018-08-06 16:57:39 +0100

    Add test for rowsFromObject

--- packages/utils/src/test.js
+++ packages/utils/src/test.js
@@ -17,3 +17,27 @@ describe('rowsFromArray', () => {
     expect(rows).toEqual([['-', 'two', '-'], ['test', 'two', '-']]);
   });
 });
+
+describe('rowsFromObject', () => {
+  it('works as expected', () => {
+    const { rowsFromObject } = exports;
+    const fields = [
+      { key: 'one', heading: 'One' },
+      { key: 'two', heading: 'Two', transform: () => 'two' },
+    ];
+    const object = { one: 'test', two: 'test' };    
+    const skipEmptyValues = true;
+    const rows = rowsFromObject(object, fields, skipEmptyValues);
+
+    expect(rows).toEqual({
+      rows: [
+        ['One', 'test'],
+        ['Two', 'two']
+      ],
+      names: [
+        'one',
+        'two',
+      ]
+    });
+  });
+});
