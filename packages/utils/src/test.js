c558fc4689269bb845df3dabf6aaa193169aca64
Gavin Orland <gavin@gavinorland.com>
2018-08-06 17:22:11 +0100

    TableUtils - function tests

--- packages/utils/src/test.js
+++ packages/utils/src/test.js
@@ -41,3 +41,63 @@ describe('rowsFromObject', () => {
     });
   });
 });
+
+describe('titlesFromFields', () => {
+  it('works as expected', () => {
+    const { titlesFromFields } = exports;
+    const fields = [
+      { key: 'one', heading: 'One' },
+      { key: 'two', title: 'Two' },
+    ];
+    const result = titlesFromFields(fields);
+    expect(result).toEqual(['One', 'Two']);
+  });
+});
+
+describe('keysFromFields', () => {
+  it('works as expected', () => {
+    const { keysFromFields } = exports;
+    const fields = [
+      { key: 'one', heading: 'One' },
+      { key: 'two', title: 'Two' },
+    ];
+    const result = keysFromFields(fields);
+    expect(result).toEqual(['one', 'two']);
+  });
+});
+
+describe('arrayHasValueForKeys', () => {
+  it('returns true when expected', () => {
+    const { arrayHasValueForKeys } = exports;
+    const keys = ['one', 'two'];
+    const array = [{ one: 'test', two: 'test' }];
+    const result = arrayHasValueForKeys(array, keys);
+    expect(result).toBe(true);
+  });
+
+  it('returns false when expected', () => {
+    const { arrayHasValueForKeys } = exports;
+    const keys = ['one', 'two'];
+    const array = [{}];
+    const result = arrayHasValueForKeys(array, keys);
+    expect(result).toBe(false);
+  });
+});
+
+describe('objectHasValueForKeys', () => {
+  it('returns true when expected', () => {
+    const { objectHasValueForKeys } = exports;
+    const keys = ['one', 'two'];
+    const item = { one: 'test', two: 'test' };
+    const result = objectHasValueForKeys(item, keys);
+    expect(result).toBe(true);
+  });
+
+  it('returns false when expected', () => {
+    const { objectHasValueForKeys } = exports;
+    const keys = ['one', 'two'];
+    const item = {};
+    const result = objectHasValueForKeys(item, keys);
+    expect(result).toBe(false);
+  });
+});
