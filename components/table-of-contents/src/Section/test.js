9953efbeb653bdb13de29557dec7ed21c5e8a6e1
Gavin Orland <gavin@gavinorland.com>
2018-08-23 14:37:45 +0100

    Check for toc before using it
    Section assumes it is being used inside a Container which wraps a Context.Provider. If it isn't, then no toc value is provided. This commit checks whether one is present before attempting to use it.
    
    It leaves in the Context.Consumer wrapper but is functional nonetheless.

--- components/table-of-contents/src/Section/test.js
+++ components/table-of-contents/src/Section/test.js
@@ -1,35 +1,71 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
-import { Entry } from '.';
+import Section, { Entry } from '.';
+
+let wrapper;
+
+describe('TableOfContents Section', () => {
+  it('renders standalone', () => {
+    wrapper = mount(
+      <Section id="id" title="Title">Section text here</Section>,
+    );
+    expect(wrapper.exists()).toBe(true);
+  });
+});
 
 describe('TableOfContents Section Entry', () => {
-  let wrapper;
-  const addContent = jest.fn();
-  const removeContent = jest.fn();
+  describe('works with Context API', () => {
+    const addContent = jest.fn();
+    const removeContent = jest.fn();
 
-  it('renders without crashing', () => {
-    wrapper = shallow(
-      <Entry id="id" title="Title" toc={{ addContent, removeContent }}>
+    it('renders without crashing', () => {
+      wrapper = shallow(
+        <Entry id="id" title="Title" toc={{ addContent, removeContent }}>
+          Section text here
+        </Entry>,
+      );
+      expect(wrapper.exists()).toBe(true);
+    });
+
+    it('has called addContent in context', () => {
+      expect(addContent.mock.calls.length).toBe(1);
+    });
+
+    it('has not yet called removeContent in context', () => {
+      expect(removeContent.mock.calls.length).toBe(0);
+    });
+
+    it('calls removeContent on remove/unmount', () => {
+      wrapper.unmount();
+      expect(removeContent.mock.calls.length).toBe(1);
+    });
+
+    it('matches snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('works without Context API', () => {
+    it('renders without crashing', () => {
+      wrapper = mount(
+        <Entry id="id" title="Title">
         Section text here
-      </Entry>,
-    );
-  });
+        </Entry>,
+      );
+      expect(wrapper.exists()).toBe(true);
+    });
 
-  it('has called addContent in context', () => {
-    expect(addContent.mock.calls.length).toBe(1);
-  });
+    it('does not find toc prop', () => {
+      expect(wrapper.prop('toc')).toBeUndefined();
+    });
 
-  it('has not yet called removeContent in context', () => {
-    expect(removeContent.mock.calls.length).toBe(0);
-  });
+    it('unmounts without issue', () => {
+      wrapper.unmount();
+    });
 
-  it('calls removeContent on remove/unmount', () => {
-    wrapper.unmount();
-    expect(removeContent.mock.calls.length).toBe(1);
-  });
-
-  it('matches snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+    it('matches snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 });
