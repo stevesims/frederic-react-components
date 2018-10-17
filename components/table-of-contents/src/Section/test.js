b4a711db0394edbbaf8b881641beca5b592eba78
Gavin Orland <gavin@gavinorland.com>
2018-10-17 17:20:36 +0100

    Improve tests

--- components/table-of-contents/src/Section/test.js
+++ components/table-of-contents/src/Section/test.js
@@ -3,11 +3,9 @@ import { mount, shallow } from 'enzyme';
 
 import Section, { Entry } from '.';
 
-let wrapper;
-
 describe('TableOfContents Section', () => {
   it('renders standalone', () => {
-    wrapper = mount(
+    const wrapper = mount(
       <Section id="id" title="Title">Section text here</Section>,
     );
     expect(wrapper.exists()).toBe(true);
@@ -16,11 +14,10 @@ describe('TableOfContents Section', () => {
 
 describe('TableOfContents Section Entry', () => {
   describe('works with Context API', () => {
-    const addContent = jest.fn();
-    const removeContent = jest.fn();
-
     it('renders without crashing', () => {
-      wrapper = shallow(
+      const addContent = jest.fn();
+      const removeContent = jest.fn();
+      const wrapper = shallow(
         <Entry id="id" title="Title" toc={{ addContent, removeContent }}>
           Section text here
         </Entry>,
@@ -29,26 +26,54 @@ describe('TableOfContents Section Entry', () => {
     });
 
     it('has called addContent in context', () => {
+      const addContent = jest.fn();
+      const removeContent = jest.fn();
+      shallow(
+        <Entry id="id" title="Title" toc={{ addContent, removeContent }}>
+          Section text here
+        </Entry>,
+      );
       expect(addContent.mock.calls.length).toBe(1);
     });
 
     it('has not yet called removeContent in context', () => {
+      const addContent = jest.fn();
+      const removeContent = jest.fn();
+      shallow(
+        <Entry id="id" title="Title" toc={{ addContent, removeContent }}>
+          Section text here
+        </Entry>,
+      );
       expect(removeContent.mock.calls.length).toBe(0);
     });
 
     it('calls removeContent on remove/unmount', () => {
+      const addContent = jest.fn();
+      const removeContent = jest.fn();
+      const wrapper = shallow(
+        <Entry id="id" title="Title" toc={{ addContent, removeContent }}>
+          Section text here
+        </Entry>,
+      );
       wrapper.unmount();
       expect(removeContent.mock.calls.length).toBe(1);
     });
 
     it('matches snapshot', () => {
+      const addContent = jest.fn();
+      const removeContent = jest.fn();
+      const wrapper = shallow(
+        <Entry id="id" title="Title" toc={{ addContent, removeContent }}>
+          Section text here
+        </Entry>,
+      );
       expect(wrapper).toMatchSnapshot();
     });
   });
 
   describe('works without Context API', () => {
     it('renders without crashing', () => {
-      wrapper = mount(
+      const wrapper = mount(
         <Entry id="id" title="Title">
           Section text here
         </Entry>,
@@ -57,14 +82,29 @@ describe('TableOfContents Section Entry', () => {
     });
 
     it('does not find toc prop', () => {
+      const wrapper = mount(
+        <Entry id="id" title="Title">
+          Section text here
+        </Entry>,
+      );
       expect(wrapper.prop('toc')).toBeUndefined();
     });
 
     it('unmounts without issue', () => {
+      const wrapper = mount(
+        <Entry id="id" title="Title">
+          Section text here
+        </Entry>,
+      );
       wrapper.unmount();
     });
 
     it('matches snapshot', () => {
+      const wrapper = mount(
+        <Entry id="id" title="Title">
+          Section text here
+        </Entry>,
+      );
       expect(wrapper).toMatchSnapshot();
     });
   });
